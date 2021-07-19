1887714afb3e3fd2f72eb2c5f46e32a189fdb336
Danijel Filipović <danijel.filipovic@outlook.com>
2021-07-19 22:12:40 +0200

    Added updating existing data.

--- app/src/main/java/dfilipovi/darkoapp/ShowActivity.kt
+++ app/src/main/java/dfilipovi/darkoapp/ShowActivity.kt
@@ -1,25 +1,37 @@
 package dfilipovi.darkoapp
 
-import android.content.Intent
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.ContentValues
 import android.database.Cursor
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.BaseColumns
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import dfilipovi.darkoapp.database.WorkContract
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ShowActivity : AppCompatActivity() {
 
 	private var mEntityId: Int = -1;
+	private val dateFormatter: DateFormat = SimpleDateFormat("dd.MM.yyyy.")
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_show)
 		initialize()
+		initializeDatePicker()
+		initializeTimePickers()
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,6 +43,11 @@ class ShowActivity : AppCompatActivity() {
 	override fun onOptionsItemSelected(item: MenuItem): Boolean {
 		return when (item.itemId) {
 			R.id.task_update -> {
+				val numberOfRowsAffected = updateEntity()
+				if (numberOfRowsAffected > 0)
+					Toast.makeText(this, "Ažurirano", Toast.LENGTH_SHORT).show()
+				else
+					Toast.makeText(this, "Ažuriranje nije provedeno.", Toast.LENGTH_SHORT).show()
 				return true
 			}
 			R.id.task_delete -> {
@@ -40,8 +57,8 @@ class ShowActivity : AppCompatActivity() {
 					finish()
 				}
 				else
-					Toast.makeText(this, "Došlo je do greške. Brisanje nije provedeno.", Toast.LENGTH_SHORT).show()
-				true
+					Toast.makeText(this, "Brisanje nije provedeno.", Toast.LENGTH_SHORT).show()
+				return true
 			}
 			else -> super.onOptionsItemSelected(item)
 		}
@@ -52,16 +69,63 @@ class ShowActivity : AppCompatActivity() {
 		if (this.mEntityId != -1) {
 			queryForEntity(this.mEntityId)?.use {
 				it.moveToNext()
-				findViewById<TextView>(R.id.show_location).text         = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_LOCATION))
-				findViewById<TextView>(R.id.show_date).text             = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_DATE))
-				findViewById<TextView>(R.id.show_time_start).text       = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_TIME_START))
-				findViewById<TextView>(R.id.show_time_end).text         = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_TIME_END))
-				findViewById<TextView>(R.id.show_vehicle).text          = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_VEHICLE))
-				findViewById<TextView>(R.id.show_kilometers_start).text = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_KM_START))
-				findViewById<TextView>(R.id.show_kilometers_end).text   = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_KM_END))
-				findViewById<TextView>(R.id.show_work_type).text        = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_WORK_TYPE))
-				findViewById<TextView>(R.id.show_work_order).text       = it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_WORK_ORDER))
+				findViewById<EditText>(R.id.show_location).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_LOCATION)))
+				findViewById<EditText>(R.id.show_date).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_DATE)))
+				findViewById<EditText>(R.id.show_time_start).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_TIME_START)))
+				findViewById<EditText>(R.id.show_time_end).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_TIME_END)))
+				findViewById<EditText>(R.id.show_vehicle).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_VEHICLE)))
+				findViewById<EditText>(R.id.show_kilometers_start).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_KM_START)))
+				findViewById<EditText>(R.id.show_kilometers_end).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_KM_END)))
+				findViewById<EditText>(R.id.show_work_type).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_WORK_TYPE)))
+				findViewById<EditText>(R.id.show_work_order).setText(it.getString(it.getColumnIndex(WorkContract.WorkEntry.ATTR_WORK_ORDER)))
 			}
+		}
+	}
+
+	private fun initializeDatePicker() {
+		val txtDate: EditText = findViewById(R.id.show_date)
+		txtDate.setOnClickListener {
+			val text: String = txtDate.text.toString()
+			val calendar: Calendar = Calendar.getInstance()
+			if (!text.isBlank()) {
+				val parsedDate = dateFormatter.parse(text)
+				calendar.timeInMillis = parsedDate.time
+			}
+
+			DatePickerDialog(
+				this,
+				{ _, year, month, dayOfMonth -> txtDate.setText("%02d.%02d.%02d.".format(dayOfMonth, month + 1, year)) },
+				calendar[Calendar.YEAR],
+				calendar[Calendar.MONTH],
+				calendar[Calendar.DAY_OF_MONTH]
+			).show()
+		}
+	}
+
+	private fun initializeTimePickers() {
+		val txtTimeStart: EditText = findViewById(R.id.show_time_start)
+		val txtTimeEnd: EditText = findViewById(R.id.show_time_end)
+		initializeTimePickerDialog(txtTimeStart)
+		initializeTimePickerDialog(txtTimeEnd)
+	}
+
+	private fun initializeTimePickerDialog(editText: EditText) {
+		editText.setOnClickListener {
+			val text: String = editText.text.toString();
+			var initialHourOfDay: Int = 12;
+			var initialMinute: Int = 0;
+			if (!text.isEmpty()) {
+				initialHourOfDay = text.substringBefore(":", "12").trim().toInt()
+				initialMinute = text.substringAfter(":", "0").trim().toInt()
+			}
+
+			TimePickerDialog(
+				this,
+				{ _, hourOfDay, minute -> editText.setText("%02d:%02d".format(hourOfDay, minute), TextView.BufferType.EDITABLE)  },
+				initialHourOfDay,
+				initialMinute,
+				true
+			).show()
 		}
 	}
 
@@ -88,11 +152,111 @@ class ShowActivity : AppCompatActivity() {
 		return cursor
 	}
 
+	private fun updateEntity(): Int {
+		var numOfRows: Int = 0
+
+		val location: String = findViewById<EditText>(R.id.show_location).text.toString()
+		val date: String = findViewById<EditText>(R.id.show_date).text.toString()
+		val timeStart: String = findViewById<EditText>(R.id.show_time_start).text.toString()
+		val timeEnd: String = findViewById<EditText>(R.id.show_time_end).text.toString()
+		val vehicle: String = findViewById<EditText>(R.id.show_vehicle).text.toString()
+		val kilometersStart: String = findViewById<EditText>(R.id.show_kilometers_start).text.toString()
+		val kilometersEnd: String = findViewById<EditText>(R.id.show_kilometers_end).text.toString()
+		val workType: String = findViewById<EditText>(R.id.show_work_type).text.toString()
+		val workOrder: String = findViewById<EditText>(R.id.show_work_order).text.toString()
+
+		val missing: ArrayList<String> = checkForMissing(location, timeStart, timeEnd, vehicle, kilometersStart, kilometersEnd, workType, workOrder)
+
+		if (missing.isNotEmpty()) {
+			showErrorDialog(missing)
+		} else {
+			val dbHelper = WorkContract.WorkDatabaseHelper(this)
+			val database = dbHelper.writableDatabase
+
+			val values = ContentValues().apply {
+				put(WorkContract.WorkEntry.ATTR_LOCATION, location)
+				put(WorkContract.WorkEntry.ATTR_DATE, date)
+				put(WorkContract.WorkEntry.ATTR_TIME_START, timeStart)
+				put(WorkContract.WorkEntry.ATTR_TIME_END, timeEnd)
+				put(WorkContract.WorkEntry.ATTR_VEHICLE, vehicle)
+				put(WorkContract.WorkEntry.ATTR_KM_START, kilometersStart)
+				put(WorkContract.WorkEntry.ATTR_KM_END, kilometersEnd)
+				put(WorkContract.WorkEntry.ATTR_WORK_TYPE, workType)
+				put(WorkContract.WorkEntry.ATTR_WORK_ORDER, workOrder)
+			}
+			val whereClause = "${BaseColumns._ID} = ?"
+			val whereArgs = arrayOf("${this.mEntityId}")
+
+			numOfRows = database.update(WorkContract.WorkEntry.ENTITY_NAME, values, whereClause, whereArgs)
+		}
+
+		return numOfRows
+	}
+
+	private fun checkForMissing(
+		location: String,
+		timeStart: String,
+		timeEnd: String,
+		vehicle: String,
+		kilometersStart: String,
+		kilometersEnd: String,
+		workType: String,
+		workOrder: String
+	): ArrayList<String> {
+		val missing: ArrayList<String> = ArrayList()
+		if (location.isBlank())
+			missing.add(getString(R.string.label_location))
+		if (timeStart.isBlank())
+			missing.add(getString(R.string.label_time) + " " + getString(R.string.label_time_start).lowercase())
+		if (timeEnd.isBlank())
+			missing.add(getString(R.string.label_time) + " " + getString(R.string.label_time_end).lowercase())
+		if (vehicle.isBlank())
+			missing.add(getString(R.string.label_vehicle))
+		if (kilometersStart.isBlank())
+			missing.add(getString(R.string.label_kilometers_start) + " " + getString(R.string.label_kilometers).lowercase())
+		if (kilometersEnd.isBlank())
+			missing.add(getString(R.string.label_kilometers_end) + " " + getString(R.string.label_kilometers).lowercase())
+		if (workType.isBlank())
+			missing.add(getString(R.string.label_work_type))
+		if (workOrder.isBlank())
+			missing.add(getString(R.string.label_work_order))
+		return missing
+	}
+
+	private fun showErrorDialog(missing: ArrayList<String>) {
+		val messageView: LinearLayout = LinearLayout(this)
+		messageView.orientation = LinearLayout.VERTICAL
+		messageView.setPadding(73, 24, 73, 24)
+
+		val tvMessage = TextView(this)
+		tvMessage.text = getString(R.string.error_incomplete_fields_message)
+		tvMessage.setTextColor(getColor(R.color.black))
+		tvMessage.setPadding(0, 0, 0, 5)
+		messageView.addView(tvMessage)
+
+		for (item in missing) {
+			val tvItem = TextView(this)
+			tvItem.text = item;
+			tvItem.setTextColor(getColor(R.color.black))
+			tvItem.setPadding(30, 5, 0, 5)
+			messageView.addView(tvItem)
+		}
+
+		AlertDialog.Builder(this)
+			.setTitle(getString(R.string.error_incomplete_fields_title))
+			.setView(messageView)
+			.setPositiveButton("OK", null)
+			.create()
+			.show()
+	}
+
 	private fun deleteEntity(): Int {
 		val dbHelper = WorkContract.WorkDatabaseHelper(this)
 		val database = dbHelper.writableDatabase
+
 		val whereClause = "${BaseColumns._ID} = ?"
 		val whereArgs = arrayOf("${this.mEntityId}")
+
 		return database.delete(WorkContract.WorkEntry.ENTITY_NAME, whereClause, whereArgs)
 	}
 }
